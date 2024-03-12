d5184c636ca8c0e1a73bc245f140b3bce345ee05
MICHAELL� MAIARA VITORINO <michaelly.3959@aluno.pr.senac.br>
2024-03-12 15:54:45 -0300

    atualizando-1

--- truthy-Falsy.js
+++ truthy-Falsy.js
@@ -9,6 +9,20 @@ const contapaga = false
 
 console.log(0 == false)
 console.log(" " == false)
+console.log(1 == true)
 
-// null
 // underfined
+
+// null ==> vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3
+let texto = "Alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
