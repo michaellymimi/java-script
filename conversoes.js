51495218390787ab342104647f3ba67c77284327
MICHAELL� MAIARA VITORINO <michaelly.3959@aluno.pr.senac.br>
2024-03-19 15:04:44 -0300

    primiro commit

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dado 
+//booleanos
+
+//conversão implicita
+const numero = 456;
+const numeroString = 456;
+
+
+//Number()
+//String()
+console.log(numero + numeroString)
+
+// conversão explicita 
