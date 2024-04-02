c54bb43749a56f515148334f244e3f8c8ed04ea5
MICHAELL� MAIARA VITORINO <michaelly.3959@aluno.pr.senac.br>
2024-04-02 15:53:55 -0300

    novo

--- parametros.js
+++ parametros.js
@@ -1,9 +1,34 @@
 // parâmetros de função
 
+//function soma(numero1, numero2) {
+    //return numero1 + numero2;
+//}
+
+//console.log(soma(2, 2))
+//console.log(soma(245, 20))
+//console.log(soma(-500, 60))
+
+//paramêtros x argumentos 
+
+//ordem dos paramêtros
+
+
+
+//function nomeIdade(nome, idade) {
+  //return 'meu nome é ${nome} e minha idade é ${idade}';
+//}
+
+//console.log(nomeIdade(16, "michaelly"))
+
+
 function soma(numero1, numero2) {
     return numero1 + numero2;
 }
 
-console.log(soma(2, 2))
-console.log(soma(245, 20))
-console.log(soma(-500, 60))
+function multiplicação(numero1 = 1, numero2 = 1) {
+    return numero1 * numero2;
+}
+
+console.log(multiplicação(soma(4, 5)))
+
+
