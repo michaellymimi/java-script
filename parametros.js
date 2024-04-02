02e2e2d9f1e7a58bb3eec55aa3a2285473610c97
MICHAELL� MAIARA VITORINO <michaelly.3959@aluno.pr.senac.br>
2024-04-02 14:52:53 -0300

    novo

---
+++ parametros.js
@@ -0,0 +1,9 @@
+// parâmetros de função
+
+function soma(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+console.log(soma(2, 2))
+console.log(soma(245, 20))
+console.log(soma(-500, 60))
---
+++ template-string.js
@@ -0,0 +1,8 @@
+const nome = "Ju";
+const idade = 2024-2008
+const cidadeDeNascimento = "Arapongas"
+
+// const apresentacao = " meu nome é " + nome + ,minha idade é" + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao =  `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+console.log(apresentacao)
